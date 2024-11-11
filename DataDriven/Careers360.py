4c9d69d1ce719f72c611ff44fb9e8b75d19a8778
mohdimranQA <163973632+mohdimranQA@users.noreply.github.com>
2024-11-11 14:49:03 +0530

    Update3 Careers360.py

--- DataDriven/Careers360.py
+++ DataDriven/Careers360.py
@@ -11,4 +11,5 @@ driver.minimize_window()
 driver.maximize_window()
 time.sleep(5)
 path="/home/imran/Documents/Python/DataDriven/TestData.xlsx"
+time.sleep(5)
 rows = XLUtils.getRowsCount(path,"Sheet1")
